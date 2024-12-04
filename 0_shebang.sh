98fa39d4384e9a1f9ba4ea730e49a93f2c9cf52e
DESKTOP-HHV3M9S\wontae <wontae.gramin9@gmail.com>
2024-12-04 14:39:41 +0000

    [Shebang(#!)] Tell script which interpret to use

---
+++ 0_shebang.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# shebang(#!): tell script which interpret to use
+
+# without a shebang, I need to invoke the interpreter manually
+# bash script.sh
+# defaults to usually, bin/sh (bourne)
+
+# with a shebang, I can just execute
+# ./script.sh
+
+
+
+echo Hello World!
+echo $SHELL
+# bash에서 환경변수에 접근하는 법 $
+
