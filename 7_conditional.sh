0c28fa51ce6dba81bf2cb67c27b4a319ed371b9b
DESKTOP-HHV3M9S\wontae <wontae.gramin9@gmail.com>
2024-12-05 16:44:06 +0000

    [Conditional expressions] Case statement(switch statement)

--- 7_conditional.sh
+++ 7_conditional.sh
@@ -8,10 +8,22 @@ else
   echo "You're not the boss"
 fi 
 
-
 # if [] ← if + test command
 # ${1,,} = first positional argument
 
 # $1 means first command line arg
 # {arg,,} ← parameter expansion converting to lowecase
 # if $1 is WONTAE, it becomes wontae
+
+case ${1,,} in
+  # OR | 
+  wontae | administrator) 
+    echo "You're the boss here."
+    ;; # means break
+  help)
+    echo "Enter your usernmae"
+    ;; 
+  *) # catch all (default)
+    echo "You're not the boss"
+    ;; 
+esac # close case statement with case reverted
