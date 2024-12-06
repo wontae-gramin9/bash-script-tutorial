0a93bf0b9d266b4b1f8d1419d3b62086291b1d98
DESKTOP-HHV3M9S\wontae <wontae.gramin9@gmail.com>
2024-12-06 20:40:58 +0000

    [Function & positional argument] 함수의 괄호 안에 args를 정의하지 않고 indexing으로, 1부터 시작한다. 0은 실행되는 파일명에 예약되어있다

--- 10_functionposargu.sh
+++ 10_functionposargu.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-showname() {
+showname(){
   echo hello $1
-  # 함수의 괄호 안에 args를 정의하지는 않는다
-  # 0부터 시작하지 않고 1부터 시작한다
-  # 0은 실행되는 파일명에 예약되어있다
-
-
+  if [ ${1,,} = wontae ]; then
+    return 0
+  else
+    return 1
+  fi
 }
 
-showname Wontae
+showname $1
+# wontae에서 $1로 바꾸면 command line arguments에서 받는
+# positional argument가 된다
+
+if [ $? -ne 0 ]; then
+  # $?: 가장 마지막에 실행된 코드(여기서는 showname함수)
+  # 의 exit status를 가진다
+  # 0은 에러없이 성공적으로 실행
+  # 비0은 에러(실패)
+
+  echo "Someone unknown called the function"
+fi
